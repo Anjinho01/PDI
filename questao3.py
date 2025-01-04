563daf42b8c3dbc68637fe69c83e353ded1c5050
miguel <anjomm04@gmail.com>
2025-01-03 22:47:43 -0300

    adicionados

---
+++ questao3.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+
+def uniao(imagem1: str, imagem2: str) -> np.array:
+    array1 = cv2.imread(imagem1)
+    array2 = cv2.imread(imagem2)
+
+    retorno = []
+    for i in range(len(array1)):
+        linha = []
+        for j in range(len(array1[0])):
+            linha.append(max(array1[i][j][0], array2[i][j][0]))
+        retorno.append(linha)
+    return np.array(retorno)
+
+def intersecao(imagem1: str, imagem2: str) -> np.array:
+    array1 = cv2.imread(imagem1)
+    array2 = cv2.imread(imagem2)
+
+    retorno = []
+    for i in range(len(array1)):
+        linha = []
+        for j in range(len(array1[0])):
+            linha.append(min(array1[i][j][0], array2[i][j][0]))
+        retorno.append(linha)
+    return np.array(retorno)
+
+def diferenca(imagem1: str, imagem2: str) -> np.array:
+    array1 = cv2.imread(imagem1)
+    array2 = cv2.imread(imagem2)
+
+    retorno = []
+    for i in range(len(array1)):
+        linha = []
+        for j in range(len(array1[0])):
+            linha.append(max(array1[i][j][0] - array2[i][j][0], 0))
+        retorno.append(linha)
+    return np.array(retorno)
+
+cv2.imshow("uniao1e2", uniao("imagem1.bmp", "imagem2.bmp"))
+cv2.imshow("intersecao1e3", intersecao("imagem1.bmp", "imagem3.bmp"))
+cv2.imshow("diferenca2e3", diferenca("imagem2.bmp", "imagem3.bmp"))
+            

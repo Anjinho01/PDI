379c69cc6eb478ce16c0aeeb5a8ec94e9e74c5e4
miguel <anjomm04@gmail.com>
2025-01-03 21:50:26 -0300

    2 feitas

---
+++ questao2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+mascara1 = np.array([[  0, 1/5,   0],
+                     [1/5, 1/5, 1/5],
+                     [  0, 1/5,   0]])
+mascara2 = np.array([[1/9, 1/9, 1/9],
+                     [1/9, 1/9, 1/9],
+                     [1/9, 1/9, 1/9]])
+mascara3 = np.array([[1/32, 3/32, 1/32],
+                     [3/32, 16/32, 3/32],
+                     [1/32, 3/32, 1/32]])
+mascara4 = np.array([[  0, 1/8,   0],
+                     [1/8, 4/8, 1/8],
+                     [  0, 1/8,   0]])
+
+imagem = cv2.imread("lena_ruido.bmp")
+
+
+cv2.imshow("original", imagem)
+cv2.imshow("mascara 1", cv2.filter2D(imagem, -1, mascara1))
+cv2.imshow("mascara 2", cv2.filter2D(imagem, -1, mascara2))
+cv2.imshow("mascara 3", cv2.filter2D(imagem, -1, mascara3))
+cv2.imshow("mascara 4", cv2.filter2D(imagem, -1, mascara4))
+cv2.imshow("mediana", cv2.medianBlur(imagem, 3))
